bbc29ce077e4e8b3ae01aa2aa1e574c7942b64f7
ProfTahseen <tahsinalpaydin@gmail.com>
2024-08-17 03:38:04 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import asyncio
+import websockets
+
+
+REGISTERS = ["0", "0", "0", "0", "0", "0", "0", "0"]
+
+async def stuff(websocket):
+	global REGISTERS
+
+	async for request in websocket:
+
+		if request == 'read':
+
+			await websocket.send(''.join(REGISTERS))
+			print(f'READ!: {' - '.join(REGISTERS)}')
+
+		else:
+
+			REGISTERS = list(request)[0:8]
+			await websocket.send('ok')
+			print(f'WRITTEN!: {' - '.join(REGISTERS)}')
+
+
+async def main():
+	async with websockets.serve(stuff, "localhost", 8765):
+		await asyncio.Future()  # run forever
+
+if __name__ == "__main__":
+	asyncio.run(main())
